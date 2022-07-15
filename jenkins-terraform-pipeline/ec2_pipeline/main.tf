e550f37bc658e6953f27fe4dd5603cb90e8164d0
mustafanu82 <82030444+mustafanu82@users.noreply.github.com>
2022-07-15 16:27:05 +0000

    Update manin.tf

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -1,8 +1,21 @@
-provider "aws" {
-  region = "us-west-2"
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0721c9af7b9b75114"
-  instance_type = "t2.nano"
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
 }
